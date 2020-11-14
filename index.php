50d44c619203a57a3ca6e7b69295082770241b5b
maikl <dzion67@mail.ru>
2020-11-14 15:51:55 +0300

    Update head html
    Signed-off-by: maikl <dzion67@mail.ru>

--- index.php
+++ index.php
@@ -29,40 +29,20 @@ $r['factory'] = initFactory();
     <meta name="keywords" content=""/>
     <meta name="author" content=""/>
 
-    <!-- Facebook and Twitter integration -->
-    <meta property="og:title" content=""/>
-    <meta property="og:image" content=""/>
-    <meta property="og:url" content=""/>
-    <meta property="og:site_name" content=""/>
-    <meta property="og:description" content=""/>
-    <meta name="twitter:title" content=""/>
-    <meta name="twitter:image" content=""/>
-    <meta name="twitter:url" content=""/>
-    <meta name="twitter:card" content=""/>
-
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
     <link rel="shortcut icon" href="favicon.ico">
 
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
 
-    <!-- Animate.css -->
     <link rel="stylesheet" href="assets/css/animate.css">
-    <!-- Icomoon Icon Fonts-->
     <link rel="stylesheet" href="assets/css/icomoon.css">
-    <!-- Bootstrap  -->
     <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <!-- Flexslider  -->
     <link rel="stylesheet" href="assets/css/flexslider.css">
-    <!-- Flaticons  -->
     <link rel="stylesheet" href="assets/fonts/flaticon/font/flaticon.css">
-    <!-- Owl Carousel -->
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
-    <!-- Theme style  -->
     <link rel="stylesheet" href="assets/css/style.css">
-
-    <!-- Modernizr JS -->
     <script src="assets/js/modernizr-2.6.2.min.js"></script>
     <!-- FOR IE9 below -->
     <!--[if lt IE 9]>
